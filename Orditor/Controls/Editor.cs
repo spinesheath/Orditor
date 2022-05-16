a6f96cc21fa5964884cede3f18c876b67d3a4d42
Johannes Heckl <heckl.johannes@googlemail.com>
2022-05-17 00:45:52 +0200

    Foldings for homes

--- Orditor/Controls/Editor.cs
+++ Orditor/Controls/Editor.cs
@@ -1,9 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Folding;
 
 namespace Orditor.Controls;
+
+internal class FoldingStrategy
+{
+  private readonly FoldingManager _foldingManager;
+
+  public FoldingStrategy(FoldingManager foldingManager)
+  {
+    _foldingManager = foldingManager;
+  }
+
+  public void UpdateFoldings(TextDocument document)
+  {
+    var foldings = SafeCreateNewFoldings(document, out var firstErrorOffset);
+    _foldingManager.UpdateFoldings(foldings, firstErrorOffset);
+  }
+
+  public IEnumerable<NewFolding> SafeCreateNewFoldings(TextDocument document, out int firstErrorOffset)
+  {
+    try
+    {
+      return CreateNewFoldings(document, out firstErrorOffset);
+    }
+    catch
+    {
+      firstErrorOffset = 0;
+      return Enumerable.Empty<NewFolding>();
+    }
+  }
+
+  public IEnumerable<NewFolding> CreateNewFoldings(TextDocument document, out int firstErrorOffset)
+  {
+    var stack = new Stack<NewFolding>();
+    var foldMarkers = new List<NewFolding>();
+    var index = 0;
+
+    if (document.Lines.Count < 2)
+    {
+      firstErrorOffset = -1;
+      return Enumerable.Empty<NewFolding>();
+    }
+
+    try
+    {
+      var prefaceFolding = new NewFolding(0, 0);
+      prefaceFolding.Name = "preface";
+      stack.Push(prefaceFolding);
+      var lines = document.Lines;
+      var previousLine = lines[0];
+      for (; index < lines.Count; index++)
+      {
+        var line = lines[index];
+        var trimmed = document.GetText(line).Trim();
+        if (trimmed.StartsWith("home"))
+        {
+          var previous = stack.Pop();
+          previous.EndOffset = previousLine.EndOffset;
+          foldMarkers.Add(previous);
+          
+          var homeFolding = new NewFolding(line.Offset, line.EndOffset);
+          homeFolding.Name = trimmed;
+          stack.Push(homeFolding);
+        }
+
+        previousLine = line;
+      }
+
+      firstErrorOffset = -1;
+    }
+    catch
+    {
+      if (index >= 1 && index <= document.LineCount)
+        firstErrorOffset = document.GetOffset(index, 0);
+      else
+        firstErrorOffset = 0;
+    }
+
+    foldMarkers.Sort((a, b) => a.StartOffset.CompareTo(b.StartOffset));
+    return foldMarkers;
+  }
+}
 
 internal class Editor : Decorator
 {
@@ -11,6 +95,8 @@ internal class Editor : Decorator
   {
     Initialized += SetChild;
     _textEditor.TextChanged += OnTextChangedInternal;
+    var foldingManager = FoldingManager.Install(_textEditor.TextArea);
+    _foldingStrategy = new FoldingStrategy(foldingManager);
   }
 
   public int FocusedLineIndex
@@ -32,6 +118,7 @@ internal class Editor : Decorator
     nameof(FocusedLineIndex), typeof(int), typeof(Editor), new PropertyMetadata(default(int), OnLineIndexChanged));
 
   private readonly TextEditor _textEditor = new();
+  private readonly FoldingStrategy _foldingStrategy;
 
   private static void OnLineIndexChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
   {
@@ -59,6 +146,7 @@ internal class Editor : Decorator
     if (_textEditor.Text != Text)
     {
       _textEditor.Text = Text;
+      _foldingStrategy.UpdateFoldings(_textEditor.Document);
     }
   }
 
