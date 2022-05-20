8a3157889b0ae8d0de4241908187681a3ec4e2de
Johannes Heckl <heckl.johannes@googlemail.com>
2022-05-20 15:14:02 +0200

    Refactoring

---
+++ Orditor/Model/Annotations.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Orditor.Model;
+
+internal class Annotations
+{
+  public Annotations(string path, PickupGraph graph)
+  {
+    _annotationsPath = path;
+    _graph = graph;
+    _annotations = ReadAnnotations();
+    CalculateHomeLocations();
+  }
+
+  public Vector Location(Home home)
+  {
+    var homeElement = Annotation(home);
+    var x = homeElement?.Attribute("x")?.Value;
+    var y = homeElement?.Attribute("y")?.Value;
+
+    if (x == null || y == null)
+    {
+      return CalculatedLocation(home);
+    }
+
+    return new Vector(Convert.ToDouble(x, CultureInfo.InvariantCulture), Convert.ToDouble(y, CultureInfo.InvariantCulture));
+  }
+
+  public void SetLocation(Home home, Vector gamePosition)
+  {
+    if (double.IsNaN(gamePosition.X) || double.IsNaN(gamePosition.Y))
+    {
+      return;
+    }
+
+    var homeElement = _annotations.Elements("home").FirstOrDefault(e => e.Attribute("name")?.Value == home.Name);
+    if (homeElement == null)
+    {
+      var name = new XAttribute("name", home.Name);
+      var x = new XAttribute("x", gamePosition.X.ToString(CultureInfo.InvariantCulture));
+      var y = new XAttribute("y", gamePosition.Y.ToString(CultureInfo.InvariantCulture));
+      var xElement = new XElement("home", name, x, y);
+      _annotations.Add(xElement);
+    }
+    else
+    {
+      homeElement.SetAttributeValue("x", gamePosition.X.ToString(CultureInfo.InvariantCulture));
+      homeElement.SetAttributeValue("y", gamePosition.Y.ToString(CultureInfo.InvariantCulture));
+    }
+
+    _annotations.Save(_annotationsPath);
+  }
+
+  private readonly XElement _annotations;
+
+  private readonly string _annotationsPath;
+  private readonly Dictionary<Home, Vector> _calculatedLocations = new();
+  private readonly PickupGraph _graph;
+
+  private Vector CalculatedLocation(Home home)
+  {
+    return _calculatedLocations.TryGetValue(home, out var location) ? location : new Vector(double.NaN, double.NaN);
+  }
+
+  private XElement? Annotation(Home home)
+  {
+    return _annotations.Elements("home").FirstOrDefault(e => e.Attribute("name")?.Value == home.Name);
+  }
+
+  private XElement ReadAnnotations()
+  {
+    return File.Exists(_annotationsPath) ? XElement.Load(_annotationsPath) : new XElement("annotations");
+  }
+
+  private void CalculateHomeLocations()
+  {
+    var noPickups = new Queue<Home>();
+    foreach (var home in _graph.Homes)
+    {
+      var pickups = _graph.GetPickups(home).ToList();
+      if (pickups.Count == 0)
+      {
+        noPickups.Enqueue(home);
+      }
+      else if (pickups.Count == 1)
+      {
+        var pickup = pickups[0];
+        _calculatedLocations.Add(home, new Vector(pickup.X - 20, pickup.Y));
+      }
+      else
+      {
+        var x = pickups.Sum(p => p.X) / pickups.Count;
+        var y = pickups.Sum(p => p.Y) / pickups.Count;
+        _calculatedLocations.Add(home, new Vector(x, y));
+      }
+    }
+
+    while (noPickups.Count > 0)
+    {
+      var home = noPickups.Dequeue();
+      var connectedHomes = _graph.GetConnectedHomes(home).ToList();
+      if (connectedHomes.Count == 0)
+      {
+        _calculatedLocations.Add(home, new Vector(50, 50));
+        continue;
+      }
+
+      var withLocations = connectedHomes.Where(h => _calculatedLocations.ContainsKey(h));
+      var locations = withLocations.Select(h => _calculatedLocations[h]).ToList();
+
+      if (locations.Count == 0)
+      {
+        noPickups.Enqueue(home);
+      }
+      else if (locations.Count == 1)
+      {
+        var pickup = locations[0];
+        _calculatedLocations.Add(home, new Vector(pickup.X + 20, pickup.Y));
+      }
+      else
+      {
+        var x = locations.Sum(p => p.X) / locations.Count;
+        var y = locations.Sum(p => p.Y) / locations.Count;
+        _calculatedLocations.Add(home, new Vector(x, y));
+      }
+    }
+  }
+}
--- Orditor/Model/World.cs
+++ Orditor/Model/World.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
 using System.Windows;
-using System.Xml.Linq;
 
 namespace Orditor.Model;
 
@@ -15,8 +12,7 @@ internal class World
     var parser = ParseAreas();
     _graph = parser.Graph;
     _file = parser.File;
-    _annotations = ReadAnnotations();
-    CalculateHomeLocations();
+    _annotations = new Annotations(AnnotationsPath, _graph);
   }
 
   public IEnumerable<Home> Homes => _graph.Homes;
@@ -34,16 +30,7 @@ internal class World
 
   public Vector Location(Home home)
   {
-    var homeElement = Annotation(home);
-    var x = homeElement?.Attribute("x")?.Value;
-    var y = homeElement?.Attribute("y")?.Value;
-
-    if (x == null || y == null)
-    {
-      return CalculatedLocation(home);
-    }
-
-    return new Vector(Convert.ToDouble(x, CultureInfo.InvariantCulture), Convert.ToDouble(y, CultureInfo.InvariantCulture));
+    return _annotations.Location(home);
   }
 
   public string RawText()
@@ -53,109 +40,18 @@ internal class World
 
   public void SetLocation(Home home, Vector gamePosition)
   {
-    if (double.IsNaN(gamePosition.X) || double.IsNaN(gamePosition.Y))
-    {
-      return;
-    }
-
-    var homeElement = _annotations.Elements("home").FirstOrDefault(e => e.Attribute("name")?.Value == home.Name);
-    if (homeElement == null)
-    {
-      var name = new XAttribute("name", home.Name);
-      var x = new XAttribute("x", gamePosition.X.ToString(CultureInfo.InvariantCulture));
-      var y = new XAttribute("y", gamePosition.Y.ToString(CultureInfo.InvariantCulture));
-      var xElement = new XElement("home", name, x, y);
-      _annotations.Add(xElement);
-    }
-    else
-    {
-      homeElement.SetAttributeValue("x", gamePosition.X.ToString(CultureInfo.InvariantCulture));
-      homeElement.SetAttributeValue("y", gamePosition.Y.ToString(CultureInfo.InvariantCulture));
-    }
-
-    _annotations.Save(AnnotationsPath);
+    _annotations.SetLocation(home, gamePosition);
   }
 
-  private const string AreasOri = "areas.ori";
+  private static readonly string AreasOriPath = GetPath("areas.ori");
   private static readonly string AnnotationsPath = GetPath("annotations.xml");
-  private readonly XElement _annotations;
-  private readonly Dictionary<Home, Vector> _calculatedLocations = new();
+  private readonly Annotations _annotations;
   private readonly StructuredFile _file;
   private readonly PickupGraph _graph;
 
-  private Vector CalculatedLocation(Home home)
-  {
-    return _calculatedLocations.TryGetValue(home, out var location) ? location : new Vector(double.NaN, double.NaN);
-  }
-
-  private XElement? Annotation(Home home)
-  {
-    return _annotations.Elements("home").FirstOrDefault(e => e.Attribute("name")?.Value == home.Name);
-  }
-
-  private static XElement ReadAnnotations()
-  {
-    return File.Exists(AnnotationsPath) ? XElement.Load(AnnotationsPath) : new XElement("annotations");
-  }
-
-  private void CalculateHomeLocations()
-  {
-    var noPickups = new Queue<Home>();
-    foreach (var home in Homes)
-    {
-      var pickups = _graph.GetPickups(home).ToList();
-      if (pickups.Count == 0)
-      {
-        noPickups.Enqueue(home);
-      }
-      else if (pickups.Count == 1)
-      {
-        var pickup = pickups[0];
-        _calculatedLocations.Add(home, new Vector(pickup.X - 20, pickup.Y));
-      }
-      else
-      {
-        var x = pickups.Sum(p => p.X) / pickups.Count;
-        var y = pickups.Sum(p => p.Y) / pickups.Count;
-        _calculatedLocations.Add(home, new Vector(x, y));
-      }
-    }
-
-    while (noPickups.Count > 0)
-    {
-      var home = noPickups.Dequeue();
-      var connectedHomes = _graph.GetConnectedHomes(home).ToList();
-      if (connectedHomes.Count == 0)
-      {
-        _calculatedLocations.Add(home, new Vector(50, 50));
-        continue;
-      }
-
-      var withLocations = connectedHomes.Where(h => _calculatedLocations.ContainsKey(h));
-      var locations = withLocations.Select(h => _calculatedLocations[h]).ToList();
-
-      if (locations.Count == 0)
-      {
-        noPickups.Enqueue(home);
-      }
-      else if (locations.Count == 1)
-      {
-        var pickup = locations[0];
-        _calculatedLocations.Add(home, new Vector(pickup.X + 20, pickup.Y));
-      }
-      else
-      {
-        var x = locations.Sum(p => p.X) / locations.Count;
-        var y = locations.Sum(p => p.Y) / locations.Count;
-        _calculatedLocations.Add(home, new Vector(x, y));
-      }
-    }
-  }
-
   private static PickupGraphParser ParseAreas()
   {
-    var path = GetPath(AreasOri);
-    var text = File.Exists(path) ? File.ReadAllText(path) : string.Empty;
+    var text = File.Exists(AreasOriPath) ? File.ReadAllText(AreasOriPath) : string.Empty;
     return new PickupGraphParser(text);
   }
 
