be4c1195997232dd0071d98b44aa0c1fa80071ef
Johannes Heckl <heckl.johannes@googlemail.com>
2022-05-28 16:33:10 +0200

    Icons for skills

---
+++ Orditor/Controls/InventoryItemNameToImagePathConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Orditor.Controls;
+
+internal class InventoryItemNameToImagePathConverter : IValueConverter
+{
+  public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    if (value is string s)
+    {
+      return $"/Data/{s.ToLowerInvariant()}.png";
+    }
+
+    return DependencyProperty.UnsetValue;
+  }
+
+  public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+  {
+    return DependencyProperty.UnsetValue;
+  }
+}
